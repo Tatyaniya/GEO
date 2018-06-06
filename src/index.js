d0c04044f901db1beea3f1a16bf9d5c64d322869
Tatiana Melnychuk <afromyak13@gmail.com>
2018-06-07 02:15:18 +0300

    add map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+ymaps.ready(init);
+    var myMap;
+        myPlacemark;
+
+    function init(){
+        myMap = new ymaps.Map("map", {
+            center: [50.45, 30.52],
+            zoom: 5
+        });
+
+        myPlacemark = new ymaps.Placemark([50.45, 30.52],
+            { preset: 'islands#blueHomeCircleIcon' });
+
+        myMap.geoObjects.add(myPlacemark);
+    }
+
